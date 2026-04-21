f2a30c540be7db5302698fc0b859af476c7a24e8
Pollo30 <vmsan30@gmail.com>
2026-04-21 09:11:00 -0600

    Change in app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "¡Pipeline actualizado exitosamente! v2.0"})
+    return jsonify({"message": "¡Pipeline actualizado exitosamente! v2.2"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

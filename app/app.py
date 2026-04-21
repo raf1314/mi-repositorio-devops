fff2d365c42e17cadc23b3156c95b4e40a84436d
Raf1ta <AL05077696@tecmilenio.mx>
2026-04-20 20:26:53 -0600

    Actualizar mensaje de la app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "¡Hola desde Flask en Docker!"})
+    return jsonify({"message": "¡Pipeline actualizado exitosamente! v2.0"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
